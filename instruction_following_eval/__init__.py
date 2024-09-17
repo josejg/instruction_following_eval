d39c9f75525a035b836c1cb7e22f0c0c4c41ad42
Jose Javier <26491792+josejg@users.noreply.github.com>
2024-09-18 01:44:01 +0200

    fix

--- instruction_following_eval/__init__.py
+++ instruction_following_eval/__init__.py
@@ -15,8 +15,8 @@ def ensure_nltk_resource():
         # If exception is raised, it means 'punkt' is not available
         # Download 'punkt'
         nltk.download("punkt")
+    nltk.download('punkt_tab')
 
 
 # Ensure NLTK resource is available before proceeding
 ensure_nltk_resource()
-nltk.download('punkt_tab')
